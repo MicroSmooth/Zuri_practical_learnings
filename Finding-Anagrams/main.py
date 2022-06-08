50afdbe640b63bff4121407770b25a33711d96a1
Thompson Smooth OKOYEN <105185847+MicroSmooth@users.noreply.github.com>
2022-06-08 23:37:51 +0100

    Update main.py

--- Finding-Anagrams/main.py
+++ Finding-Anagrams/main.py
@@ -4,11 +4,25 @@
 # find_anagrams("below", "elbow") --> True
 
 
-def find_anagram(word, anagram):
+from dataclasses import replace
+from distutils.command.check import SilentReporter
+
+
+def find_anagrams(word, anagram):
     # [assignment] Add your code here
-    if (sorted("word")==sorted("anagram")):
-        print()
 
-    return True
-print(find_anagram)
+    #change word to lowercase
+    word = word.lower()
+    anagrama = anagram.lower()
 
+    #remove whitespace
+    word = word.replace(" ", "")
+    anagram = anagram.replace(" ", "")
+
+    #sorted(word) == sorted(anagram) #true or false
+
+    return sorted(word) == sorted(anagram)
+
+print("calling find_anagrams with 'silent' and 'listen' ", find_anagrams("silent", "listen"))
+print("calling find_anagrams with 'elvis' and 'lives' ", find_anagrams("elvis", "lives"))
+print("calling find_anagrams with 'lovers' and 'hovers' ", find_anagrams("lovers", "hovers"))
